13beab35db29714b383d57a6593f00974b888360
jasonwynn10 <jasonwynn10@gmail.com>
2017-02-18 17:32:48 -0500

    added a bit more

--- src/jasonwynn10/Main.php
+++ src/jasonwynn10/Main.php
@@ -1,22 +1,28 @@
 <?php
 namespace jasonwynn10;
+
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
-use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\Player;
 
 class Main extends PluginBase {
     public function onEnable() {
         $this->saveDefaultConfig();
-        // $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->notice(TF::GREEN."Enabled!");
     }
 
     public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
         if(strtolower($command) == "code") {
-            //
+            if($sender instanceof Player) {
+                if(isset($args)) {
+                    if(strtolower($args[0]) == "reset") {
+                        #
+                    }
+                }
+                #
+            }
         }
         return true;
     }
