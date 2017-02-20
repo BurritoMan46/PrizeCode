f5a801b51c03ff633542fbc99205168055a06e7a
jasonwynn10 <jasonwynn10@gmail.com>
2017-02-20 14:09:52 -0500

    Finished item giving code

--- src/jasonwynn10/Main.php
+++ src/jasonwynn10/Main.php
@@ -1,6 +1,9 @@
 <?php
 namespace jasonwynn10;
 
+use pocketmine\item\Item;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\StringTag;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
@@ -12,16 +15,30 @@ class Main extends PluginBase {
         $this->saveDefaultConfig();
         $this->getLogger()->notice(TF::GREEN."Enabled!");
     }
-
     public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
         if(strtolower($command) == "code") {
             if($sender instanceof Player) {
                 if(isset($args)) {
-                    if(strtolower($args[0]) == "reset") {
-                        #
+                    if($args[0] == $this->getConfig()->get("CodeName","name")) {
+                        foreach ($this->getConfig()->get("Prizes",[]) as $item) {
+                            $arr = explode(":",$item);
+                            if(count($arr) == 4) {
+                                $i = Item::get($arr[0],$arr[1],$arr[2]);
+                                $i->setNamedTag(new CompoundTag("",[
+                                    new StringTag("name",$arr[3])
+                                ]));
+                            }elseif(count($arr) <= 2){
+                                $this->getLogger()->debug("Invalid item in plugin config");
+                                continue;
+                            }else{
+                                $i = Item::get($arr[0],$arr[1],$arr[2]);
+                            }
+                            $sender->getInventory()->addItem($i);
+                            return true;
+                        }
                     }
                 }
-                #
+                return false;
             }
         }
         return true;
